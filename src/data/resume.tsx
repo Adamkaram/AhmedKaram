cdb09a583630b92de7d70c60d0b238e3831fece8
AdamKaram <ahmadkarm432@gmail.com>
2024-07-07 21:47:59 +0300

    video 1

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -210,7 +210,7 @@ export const DATA = {
       ],
       image: "",
       video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+        "./ssl.mp4",
     },
     {
       title: "SSLtestify",
