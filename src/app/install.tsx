9f7ba1ff05ca941576205f77cb9643550a6288c6
AhmedKaram <52092726+Adamkaram@users.noreply.github.com>
2024-12-10 03:59:53 +0200

    Update install.tsx

--- src/app/install.tsx
+++ src/app/install.tsx
@@ -4,18 +4,11 @@ export default function install({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
-          fontSans.variable
-        )}
-      >
+    
        <pre style="word-wrap: break-word; white-space: pre-wrap;">
 z
          </pre>
-      </body>
-    </html>
+     
   );
 }
 
