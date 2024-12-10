2e4d7b62da4cb08e97d814b61b728afc8c8a2099
AhmedKaram <52092726+Adamkaram@users.noreply.github.com>
2024-12-10 03:48:47 +0200

    Update install.tsx

--- src/app/install.tsx
+++ src/app/install.tsx
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+# Set Alt+Q to switch input sources
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<alt>q']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<shift><alt>q']"
+
+echo "Input source switching shortcut set to Alt+Q"
